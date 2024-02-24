9fb24c312b3b22315a0dc969389e107bb2964de8
Stephan <cicchelli.stephan@gmail.com>
2024-02-24 10:56:56 -0300

    Req7

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -25,7 +25,20 @@ class ProcessSalaries(ProcessJobs):
         return max_salary
     
     def get_min_salary(self) -> int:
-        pass
+        if not self.jobs_list:
+            self.read(path)
+        
+        valid_min_salaries = [
+            int(job["min_salary"])
+            for job in self.jobs_list
+            if job.get("min_salary") and job["min_salary"].isdigit()
+        ]
+        
+        if not valid_min_salaries:
+            raise ValueError("Nenhum salário mínimo válido encontrado.")
+        
+        min_salary = min(valid_min_salaries)
+        return min_salary
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
         pass
