a58729f0c3ec99fc01d1fb85bf62fda9ee4e8cec
Stephan <cicchelli.stephan@gmail.com>
2024-02-24 09:29:37 -0300

    Req2

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,15 +1,20 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path: str) -> List[Dict]:
+        with open(path, encoding="utf-8") as file:
+            jobs = csv.DictReader(file)
+            self.jobs_list.extend(jobs)
+        return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
         pass
 
     def filter_by_multiple_criteria(self) -> List[dict]:
         pass
+
